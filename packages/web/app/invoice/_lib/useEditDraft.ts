ab0d0c3c3a8394f0b92756ecba2636d5f136555d
Max Shepel <max@undeletable.name>
2025-09-15 00:42:58 +0300

    Add missing files

---
+++ packages/web/app/invoice/_lib/useEditDraft.ts
@@ -0,0 +1,33 @@
+import { usePostMutation } from '@/src/lib/usePostMutation';
+
+const baseUrl = process.env.NEXT_PUBLIC_API_HOST;
+if (!baseUrl) {
+  throw new Error('missing NEXT_PUBLIC_API_HOST');
+}
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+if (!apiKey) {
+  throw new Error('missing NEXT_PUBLIC_API_KEY');
+}
+
+export function useEditDraft({
+  userName,
+  draftId,
+}: {
+  userName: string;
+  draftId: number;
+}) {
+  return usePostMutation<Record<string, any>, Blob>({
+    endpoint: `/api/users/${userName}/drafts/${draftId}`,
+    requestOptions: {
+      // baseUrl,
+      noJson: true,
+      blob: true,
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-Key': apiKey!,
+      },
+      fetchInit: {},
+    },
+  });
+}
